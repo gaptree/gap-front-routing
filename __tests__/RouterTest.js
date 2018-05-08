346b1cccc32a489db81b43fa8c021abc8f99ba80
ss9501 <1162882015@qq.com>
2018-05-08 14:14:20 +0800

    update

--- __tests__/RouterTest.js
+++ __tests__/RouterTest.js
@@ -5,12 +5,20 @@ test('Router', () => {
 
     router
         .add('/login', 'target login')
-        .add('/c/:companyCode', 'company target')
-        .add('/c/:companyCode/employee/:employeeCode', 'company employee');
+        .add('/c/{companyCode:[a-z0-9-]+}', 'company target')
+        .add('/c/{companyCode:[a-z0-9-]+}/employee/{employeeCode:[a-z0-9-]+}', 'company employee');
 
     const match = router.match('/c/gaptree/employee/some-one');
+    const match2 = router.match('/c/wecfin');
+
+    const params = {companyCode: 'wec', employeeCode: '1-4325435fa'};
+    const pattern = '/c/{companyCode:[a-z0-9-]+}/employee/{employeeCode:[a-z0-9-]+}';
+    const path = router.url(pattern, params);
+
+    expect(match2.action).toBe('company target');
 
     expect(match.action).toBe('company employee');
     expect(match.params.companyCode).toBe('gaptree');
     expect(match.params.employeeCode).toBe('some-one');
+    expect(path).toBe('/c/wec/employee/1-4325435fa');
 });
