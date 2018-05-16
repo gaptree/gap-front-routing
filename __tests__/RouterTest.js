537abd0589002ea06e5ab4c83ed80b33de50902d
zhanjh <zhanjh@126.com>
2018-05-16 14:27:48 +0800

    action -> handler

--- __tests__/RouterTest.js
+++ __tests__/RouterTest.js
@@ -15,9 +15,9 @@ test('Router', () => {
     const pattern = '/c/{companyCode:[a-z0-9-]+}/employee/{employeeCode:[a-z0-9-]+}';
     const path = router.url(pattern, params);
 
-    expect(match2.action).toBe('company target');
+    expect(match2.handler).toBe('company target');
 
-    expect(match.action).toBe('company employee');
+    expect(match.handler).toBe('company employee');
     expect(match.params.companyCode).toBe('gaptree');
     expect(match.params.employeeCode).toBe('some-one');
     expect(path).toBe('/c/wec/employee/1-4325435fa');
